9364da5cbfdba36f4fcf8725a6f3644ce1d433c0
Anna44856 <44856@edu.raseko.fi>
2022-03-28 11:00:14 +0300

    Lisää sulut lambda-funktioihin (tyyliseikka)

--- todoappi/src/App.js
+++ todoappi/src/App.js
@@ -14,11 +14,11 @@ export default class App extends React.Component {
 
   componentDidMount() {
     axios.get(`https://127.0.0.1:8000/api/tehtavat/`)
-      .then(res => {
+      .then((res) => {
         const iteemit = res.data;
         this.setState({ iteemit});
       })
-      .catch(error => {
+      .catch((error) => {
           this.setState({ virheViesti: error.message});
       });
   }
